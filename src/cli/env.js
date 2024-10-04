2d7736fff9043c4b4ab60d124438e1fd0dd919b6
Nargiza Sainazarova <nargizasaina@gmail.com>
2024-10-04 14:14:43 +0600

    feat: implement fs, module, cli tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+    const variables = Object
+            .keys(process.env)
+            .filter(key => key.startsWith(prefix))
+            .map(variable => `${variable}=${process.env[variable]}`);
+    console.log(variables.join('; '));
 };
 
 parseEnv();
