9cd4e9a615690c6567626972a818a5ce4ce4dd58
Nargiza Sainazarova <nargizasaina@gmail.com>
2024-10-07 13:51:31 +0600

    feat: add zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'node:stream';
+import { promisify } from 'util';
+import { join } from 'path';
+
 const compress = async () => {
-    // Write your code here 
+    const folderPath = join(process.cwd(), 'files');
+    const file = join(folderPath, 'fileToCompress.txt'); 
+    const pipe = promisify(pipeline);
+    
+    const gzip = createGzip();
+    const source = createReadStream(file);
+    const destination = createWriteStream(join(folderPath, 'archive.gz'));
+
+    try {
+        await pipe(source, gzip, destination);
+        console.log('File is compressed into archive.gz');
+    } catch (err) {
+        console.error('Error while compressing:', err.message);
+    }
 };
 
 await compress();
